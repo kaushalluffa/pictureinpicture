66f1cb49fddf77195792e7edc43321f82169f222
kaushalluffa <webdevelopment852@gmail.com>
2021-12-23 01:39:05 -0500

    final commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+const selectMediaStream = async () => {
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+        videoElement.srcObject = mediaStream
+        videoElement.onloadedmetadata = () => {
+            videoElement.play()
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
+button.addEventListener('click', async () =>{
+    button.disabled = true
+    await videoElement.requestPictureInPicture()
+    button.disabled = false
+})
+
+selectMediaStream()
